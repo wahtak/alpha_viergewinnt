44a9c2530e8bfdfcd3d872b06d848b6a819948c8
Alexander Metz <alexander.wt.metz@gmail.com>
2017-12-19 14:46:33 +0100

    add viergewinnt playing board with insert function

--- board.py
+++ board.py
@@ -1,23 +1,57 @@
+import enum
+
 import numpy as np
 
-VALUE_TO_CHAR = {0: '.', 1: 'X', 2: 'O'}
-OUTPUT_HEADER = [str(index) for index in range(1, 8)]
+
+class Stone(enum.Enum):
+    X = 1
+    O = 2
+
+
+VALUE_TO_CHAR = {0: '.', Stone.X.value: Stone.X.name, Stone.O.value: Stone.O.name}
+OUTPUT_HEADER = [str(index) for index in range(7)]
 LINE_TERMINATE = '\n'
 SEPERATOR = ' '
 
 
 class Board(object):
+    '''Generic playing board with string representation.'''
+
     def __init__(self, size):
-        self.field = np.zeros(size)
+        self.field = np.zeros(size, dtype=np.int16)
 
     def __str__(self):
         output_chars = []
         output_chars.extend([str(index + 1) for index in range(self.field.shape[1])])
         output_chars.append(LINE_TERMINATE)
-        for row in self.field:
+        for row in np.flip(self.field, axis=0):
             output_chars.extend([VALUE_TO_CHAR[cell] for cell in row])
             output_chars.append(LINE_TERMINATE)
         return SEPERATOR + SEPERATOR.join(output_chars)
+
+
+class IllegalMoveException(Exception):
+    pass
+
+
+class ViergewinntBoard(Board):
+    '''Playing board for vier gewinnt.'''
+
+    def __init__(self, *args, **kwargs):
+        super(ViergewinntBoard, self).__init__(*args, **kwargs)
+
+    def insert(self, stone, column):
+        try:
+            column_vector = self.field.T[column]
+        except IndexError:
+            raise IllegalMoveException('column %d does not exist.' % column)
+
+        for index, element in enumerate(column_vector):
+            if element == 0:
+                column_vector[index] = stone.value
+                break
+        else:
+            raise IllegalMoveException('column %d is full' % column)
 
 
 def test_board_output():
@@ -28,11 +62,29 @@ def test_board_output():
 
 def test_board_set_get():
     board = Board(size=(3, 3))
-    board.field[1, 1] = 1
-    assert board.field[1, 1] == 1
-    assert board.field[1, 0] == 0
+    board.field[0, 1] = 1
+    assert board.field[0, 1] == 1
     print(board)
 
 
+def test_viergewinnt_board_insert():
+    import pytest
+    board = ViergewinntBoard(size=(6, 7))
+
+    # valid insert
+    board.insert(stone=Stone.X, column=1)
+    assert board.field[0, 1] == Stone.X.value
+
+    # column does not exist
+    with pytest.raises(IllegalMoveException):
+        board.insert(stone=Stone.X, column=7)
+
+    # column full
+    for _ in range(6):
+        board.insert(stone=Stone.X, column=2)
+    with pytest.raises(IllegalMoveException):
+        board.insert(stone=Stone.X, column=2)
+
+
 if __name__ == '__main__':
-    test_set_get()
+    test_viergewinnt_board_insert()
