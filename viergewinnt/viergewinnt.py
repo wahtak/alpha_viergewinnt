f81658c01212be17ddd72e5930fc9891787f194e
Alexander Metz <alexander.wt.metz@gmail.com>
2017-12-25 18:26:11 +1300

    refactor: use inheritance to combine game functionality

--- viergewinnt/viergewinnt.py
+++ viergewinnt/viergewinnt.py
@@ -16,8 +16,8 @@ class ColumnFullException(Exception):
 class DropdownBoard(Board):
     '''Board with dropping stones.'''
 
-    def __init__(self, winning_condition, *args, **kwargs):
-        super(DropdownBoard, self).__init__(*args, **kwargs)
+    def __init__(self, **kwargs):
+        super(DropdownBoard, self).__init__(**kwargs)
 
     def insert(self, player, column):
         try:
@@ -32,21 +32,28 @@ class DropdownBoard(Board):
         else:
             raise ColumnFullException('column %d is full' % column)
 
-    def check_win(self, player):
-        self.winning_condition.check_win(self.state, player)
 
-
-class NStonessInRowWinningCondition(object):
+class NStonessInRowWinningCondition(Board):
     '''Winning condition checker for n stones in a row'''
 
-    def __init__(self, num_stones_in_row):
+    def __init__(self, num_stones_in_row, **kwargs):
+        super(NStonessInRowWinningCondition, self).__init__(**kwargs)
+
         horizontal_layout = np.ones(num_stones_in_row)
         vertical_layout = np.ones(num_stones_in_row).T
         diagonal_positive_layout = np.diag(np.ones(num_stones_in_row))
         diagonal_negative_layout = np.flip(np.diag(np.ones(num_stones_in_row)), axis=0)
         self.winning_layouts = [horizontal_layout, vertical_layout, diagonal_positive_layout, diagonal_negative_layout]
 
-    def check_win(self, state, player):
-        for layout in winning_layouts:
+    def check_win(self, player):
+        for layout in self.winning_layouts:
             # todo 2d convolution with state
             pass
+        return False
+
+
+class ViergewinntGame(DropdownBoard, NStonessInRowWinningCondition):
+    '''Combination of board and winning condition with parameters of the game Viergewinnt'''
+
+    def __init__(self):
+        super(ViergewinntGame, self).__init__(num_stones_in_row=4, size=(6, 7))
