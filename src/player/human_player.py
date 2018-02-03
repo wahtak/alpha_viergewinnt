f5eb625200435950d3107bc479c6cca61ba0961f
Alexander Metz <alexander.wt.metz@gmail.com>
2018-02-03 17:46:33 +0100

    add human player class

---
+++ src/player/human_player.py
@@ -0,0 +1,10 @@
+class HumanPlayer(object):
+    def get_next_move(self, state):
+        selected_move = None
+        while selected_move not in state.get_possible_moves():
+            human_input = input('select move %s: ' % state.get_possible_moves())
+            try:
+                selected_move = int(human_input)
+            except ValueError:
+                pass
+        return selected_move
