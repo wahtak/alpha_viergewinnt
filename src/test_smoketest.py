10015e60b00d2565479e84a94d734354d6b54300
Alexander Metz <alexander.wt.metz@gmail.com>
2018-02-18 02:41:52 +0100

    use mcts player in smoketest

--- src/test_smoketest.py
+++ src/test_smoketest.py
@@ -3,23 +3,40 @@ from random import Random
 from alpha_viergewinnt.game.board import Player
 from alpha_viergewinnt.game.viergewinnt import Viergewinnt, WinCondition, DrawCondition
 from alpha_viergewinnt.player.random_player import RandomPlayer
+from alpha_viergewinnt.player.mcts_player import MCTSPlayer, init_random_choice_strategy
 from alpha_viergewinnt.match import play_match
 
 
 def test_smoketest():
     # initialize random players with seeded random number generator for deterministic test
-    players = {Player.X: RandomPlayer(Random(0)), Player.O: RandomPlayer(Random(1))}
-    win_conditions = {Player.X: WinCondition(Player.X), Player.O: WinCondition(Player.O)}
-    draw_condition = DrawCondition()
+    random = Random(1)
     game = Viergewinnt()
+    player_x_win_condition = WinCondition(Player.X)
+    player_o_win_condition = WinCondition(Player.O)
+    draw_condition = DrawCondition()
+    mcts_player_x = MCTSPlayer(
+        win_condition=player_x_win_condition,
+        loss_condition=player_o_win_condition,
+        draw_condition=draw_condition,
+        selection_strategy=init_random_choice_strategy(random),
+        expansion_strategy=init_random_choice_strategy(random),
+        simulation_strategy=init_random_choice_strategy(random),
+        iterations=10,
+        rollouts=10)
 
     winner = play_match(
         game=game,
-        players=players,
-        win_conditions=win_conditions,
+        players={Player.X: mcts_player_x,
+                 Player.O: RandomPlayer(random)},
+        win_conditions={Player.X: player_x_win_condition,
+                        Player.O: player_o_win_condition},
         draw_condition=draw_condition,
-        print_state=False,
-        print_move=False,
-        print_result=False)
+        print_state=True,
+        print_move=True,
+        print_result=True)
 
     assert isinstance(winner, Player)
+
+
+if __name__ == '__main__':
+    test_smoketest()
