9f5f95a690052e6bbbb32b4d86b16dda07ebe4c8
Alexander Metz <alexander.wt.metz@gmail.com>
2018-04-06 20:34:10 +0200

    add support for tuples as input for human player

--- src/alpha_viergewinnt/player/human_player.py
+++ src/alpha_viergewinnt/player/human_player.py
@@ -1,13 +1,25 @@
+import re
+
+
 class HumanPlayer(object):
     def __init__(self, **kwargs):
         pass
 
     def get_next_move(self, state):
         selected_move = None
-        while selected_move not in state.get_possible_moves():
-            human_input = input('select move %s: ' % state.get_possible_moves())
+        possible_moves = state.get_possible_moves()
+
+        while selected_move not in possible_moves:
+            human_input = input('select move %s: ' % possible_moves)
             try:
-                selected_move = int(human_input)
+                selected_move = get_move_from_string(human_input)
             except ValueError:
                 pass
         return selected_move
+
+
+def get_move_from_string(string):
+    move = tuple(int(match) for match in re.findall('[\d]+', string))
+    if len(move) == 1:
+        return move.pop()
+    return move
