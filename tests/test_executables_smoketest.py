62b5171fbad8684b2c15e513d8dabd32078cf851
Alexander Metz <alexander.wt.metz@gmail.com>
2018-10-15 21:55:15 +0200

    skip executables tests by default

--- tests/test_executables_smoketest.py
+++ tests/test_executables_smoketest.py
@@ -1,6 +1,9 @@
 from subprocess import run
 
+import pytest
 
+
+@pytest.mark.skip()
 def test_train_smoketest():
     command = [
         'bin/train', '--game=viergewinnt', '--estimator=mlp', '--mcts-steps=3', '--training-iterations=3',
@@ -9,6 +12,7 @@ def test_train_smoketest():
     assert run(command).returncode == 0
 
 
+@pytest.mark.skip()
 def test_play_smoketest():
     command = ['bin/play', '--game=viergewinnt', '-x', 'random', '-o', 'random']
     assert run(command).returncode == 0
