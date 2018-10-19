e9eb69711b06e4ee50927896008ac02f8d3bfaef
Alexander Metz <alexander.wt.metz@gmail.com>
2018-10-19 22:29:41 +0200

    add epochs and fancy plotting to training script

--- tests/test_executables_smoketest.py
+++ tests/test_executables_smoketest.py
@@ -3,17 +3,17 @@ from subprocess import run
 import pytest
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 @pytest.mark.parametrize('game,estimator', [('viergewinnt', 'generic'), ('tictactoe', 'mlp'), ('viergewinnt', 'mlp')])
 def test_train_smoketest(game, estimator):
     command = [
-        'bin/train', '--game', game, '--estimator', estimator, '--mcts-steps=3', '--training-iterations=3',
-        '--comparison-iterations=3'
+        'bin/train', '--game', game, '--estimator', estimator, '--mcts-steps=3', '--num-training-games=3',
+        '--num-comparison-games=3', '--disable-plotting=True', '--num-epochs=3'
     ]
     assert run(command).returncode == 0
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 @pytest.mark.parametrize('game,player_x,player_o', [('viergewinnt', 'random', 'random'),
                                                     ('tictactoe', 'alpha', 'random')])
 def test_play_smoketest(game, player_x, player_o):
