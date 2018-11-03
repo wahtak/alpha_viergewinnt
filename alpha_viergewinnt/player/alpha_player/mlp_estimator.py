9a858150cdada6ead35354d6576408301e69bb65
Alexander Metz <alexander.wt.metz@gmail.com>
2018-11-03 16:02:56 +0100

    parametrize number of layers in mlp estimator

--- alpha_viergewinnt/player/alpha_player/mlp_estimator.py
+++ alpha_viergewinnt/player/alpha_player/mlp_estimator.py
@@ -2,13 +2,20 @@ import logging
 
 import torch
 from torch import tensor, tanh, sum, mean, log
-from torch.nn import Linear, Module, BatchNorm1d
+from torch.nn import Linear, Module
 from torch.nn.functional import mse_loss, softmax, relu
 from torch.optim import Adam
 
 
 class MlpEstimator(Module):
-    def __init__(self, board_size, actions, hidden_layer_scale=10, filename=None):
+    def __init__(self,
+                 board_size,
+                 actions,
+                 hidden_layer_scale=10,
+                 num_common_hidden_layers=3,
+                 num_action_hidden_layers=2,
+                 num_value_hidden_layers=2,
+                 filename=None):
         super().__init__()
         self.logger = logging.getLogger(self.__class__.__module__ + '.' + self.__class__.__name__)
 
@@ -16,28 +23,25 @@ class MlpEstimator(Module):
         board_width, board_height = board_size
         self.input_size = board_width * board_height
         self.filename = filename
+        self.num_common_hidden_layers = num_common_hidden_layers
+        self.num_action_hidden_layers = num_action_hidden_layers
+        self.num_value_hidden_layers = num_value_hidden_layers
 
-        self.hidden_size = self.input_size * hidden_layer_scale
-        self.distr_size = len(actions)
+        hidden_size = self.input_size * hidden_layer_scale
+        self.action_size = len(actions)
         self.value_size = 1
 
         # common hidden layers
-        self.fc_common1 = Linear(self.input_size, self.hidden_size)
-        self.bn_common1 = BatchNorm1d(self.hidden_size, track_running_stats=False)
-        self.fc_common2 = Linear(self.hidden_size, self.hidden_size)
-        self.bn_common2 = BatchNorm1d(self.hidden_size, track_running_stats=False)
-        self.fc_common3 = Linear(self.hidden_size, self.hidden_size)
-        self.bn_common3 = BatchNorm1d(self.hidden_size, track_running_stats=False)
+        self.fc_common_input = Linear(self.input_size, hidden_size)
+        self.fc_common_hidden = [Linear(hidden_size, hidden_size) for _ in range(self.num_common_hidden_layers)]
 
         # action distribution layers
-        self.fc_distr1 = Linear(self.hidden_size, self.hidden_size)
-        self.bn_distr1 = BatchNorm1d(self.hidden_size, track_running_stats=False)
-        self.fc_distr2 = Linear(self.hidden_size, self.distr_size)
+        self.fc_action_hidden = [Linear(hidden_size, hidden_size) for _ in range(self.num_action_hidden_layers)]
+        self.fc_action_output = Linear(hidden_size, self.action_size)
 
         # state value layers
-        self.fc_value1 = Linear(self.hidden_size, self.hidden_size)
-        self.bn_value1 = BatchNorm1d(self.hidden_size, track_running_stats=False)
-        self.fc_value2 = Linear(self.hidden_size, self.value_size)
+        self.fc_value_hidden = [Linear(hidden_size, hidden_size) for _ in range(self.num_value_hidden_layers)]
+        self.fc_value_output = Linear(hidden_size, self.value_size)
 
         self.optimizer = Adam(self.parameters(), weight_decay=5e-4)
 
@@ -50,25 +54,29 @@ class MlpEstimator(Module):
 
     def _forward(self, state):
         # common
-        common = relu(self.fc_common1(state))
-        common = relu(self.fc_common2(common))
-        common = relu(self.fc_common3(common))
+        common_hidden = relu(self.fc_common_input(state))
+        for layer in self.fc_common_hidden:
+            common_hidden = relu(layer(common_hidden))
 
         # action_distribution
-        distr = relu(self.fc_distr1(common))
-        action_distribution = softmax(self.fc_distr2(distr), dim=1)
+        action_hidden = common_hidden
+        for layer in self.fc_action_hidden:
+            action_hidden = relu(layer(action_hidden))
+        action_distribution = softmax(self.fc_action_output(action_hidden), dim=1)
         # action_distribution = normalize(sigmoid(self.fc_distr2(distr)), p=1, dim=1)
 
         # state_value
-        value = relu(self.fc_value1(common))
-        state_value = tanh(self.fc_value2(value))
+        value_hidden = common_hidden
+        for layer in self.fc_value_hidden:
+            value_hidden = relu(layer(value_hidden))
+        state_value = tanh(self.fc_value_output(value_hidden))
 
         return action_distribution, state_value
 
     def train(self, state_array, target_distribution_array, target_state_value_array):
         state = tensor(state_array).float().view(-1, self.input_size)
         target_state_value = tensor(target_state_value_array).float().view(-1, self.value_size)
-        target_distribution = tensor(target_distribution_array).float().view(-1, self.distr_size)
+        target_distribution = tensor(target_distribution_array).float().view(-1, self.action_size)
 
         self.optimizer.zero_grad()
         action_distribution, state_value = self._forward(state)
